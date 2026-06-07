7a24915d493122b1301275ed9e7495748c7c38d7
akbarsw <akbarsw@users.noreply.github.com>
2026-06-07 04:13:24 +0000

    feat: increase logo size across all pages

--- components/agri-eye-logo.tsx
+++ components/agri-eye-logo.tsx
@@ -7,12 +7,12 @@ type AgriEyeLogoProps = {
 
 export default function AgriEyeLogo({ subtitle, compact = false }: AgriEyeLogoProps) {
   const markWrapClass = compact
-    ? 'relative flex h-8 w-8 shrink-0 items-center justify-center bg-transparent sm:h-10 sm:w-10 lg:h-12 lg:w-12'
-    : 'relative flex h-16 w-16 shrink-0 items-center justify-center bg-transparent';
+    ? 'relative flex h-10 w-10 shrink-0 items-center justify-center bg-transparent sm:h-12 sm:w-12 lg:h-14 lg:w-14'
+    : 'relative flex h-20 w-20 shrink-0 items-center justify-center bg-transparent';
 
   const markImageClass = compact
-    ? 'h-10 w-10 object-contain sm:h-12 sm:w-12 lg:h-14 lg:w-14'
-    : 'h-20 w-20 object-contain';
+    ? 'h-12 w-12 object-contain sm:h-14 sm:w-14 lg:h-16 lg:w-16'
+    : 'h-24 w-24 object-contain';
 
   return (
     <span className="inline-flex items-center gap-3">
